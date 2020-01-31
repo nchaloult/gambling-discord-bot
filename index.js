971513268acb647ba91e8395c522569185832792
Nick Chaloult <npchaloult@gmail.com>
2020-01-31 16:24:05 -0500

    Bot successfully responds to messages

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('hello!');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const dc = require('discord.js');
+const client = new dc.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'hello') {
+    msg.reply('world!');
+    console.log('[INFO] Bot invoke with "hello" message. Responding with "world!"....');
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
