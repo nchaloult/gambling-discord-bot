8fd5233fdd7c598c77e8755985f6c70f80f7014b
Nick Chaloult <npchaloult@gmail.com>
2020-02-01 00:34:10 -0500

    The temporary "read" command shows all rows in currency table

--- index.js
+++ index.js
@@ -2,17 +2,38 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const dc = require('discord.js');
-const client = new dc.Client();
+const dcClient = new dc.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}`);
+const { Client } = require('pg');
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+client.connect();
+
+dcClient.on('ready', () => {
+  console.log(`Logged in as ${dcClient.user.tag}`);
 });
 
-client.on('message', msg => {
+dcClient.on('message', msg => {
   if (msg.content === 'hello') {
     msg.reply('world!');
-    console.log('[INFO] Bot invoke with "hello" message. Responding with "world!"....');
+    console.log('[INFO] Bot invoked with "hello" message. Responding with "world!"....');
+  } else if (msg.content === 'read') {
+    client.query('select * from currency;', (err, res) => {
+      if (err) {
+        msg.channel.send(err);
+        return;
+      }
+      let output = "";
+      res.rows.map(row => {
+        output += JSON.stringify(row);
+      });
+      msg.channel.send(output);
+      console.log('[INFO] Bot invoked with "read" message. Responding with all rows in the currency table....');
+      client.end();
+    });
   }
 });
 
-client.login(process.env.BOT_TOKEN);
+dcClient.login(process.env.BOT_TOKEN);
