6cceaaca66d7f4e82abe986b31f9994bd621f5e3
Nick Chaloult <npchaloult@gmail.com>
2020-02-01 16:43:01 -0500

    Added $give command

--- index.js
+++ index.js
@@ -123,8 +123,8 @@ dcClient.on('message', msg => {
                 msg.reply(`you gambled \$${gambleAmount} and lost. Your new balance is \$${newBalance}`);
               } else {
                 newBalance = balanceFloor;
-                console.log(new Date().toISOString() + ` -- [INFO] ${msg.author.username} gambled \$${gambleAmount} and lost. Their balance is \$${newBalance} -- you can't lose all of your money.`);
-                msg.reply(`you gambled \$${gambleAmount} and lost. Your new balance should be $0, but we bailed you out. Your actual new balance is \$${newBalance}`);
+                console.log(new Date().toISOString() + ` -- [INFO] ${msg.author.username} gambled \$${gambleAmount} and lost. Bailed out. Their balance is \$${newBalance}.`);
+                msg.reply(`you gambled \$${gambleAmount} and lost. We bailed you out. Your new balance is \$${newBalance}`);
               }
             }
 
@@ -144,6 +144,94 @@ dcClient.on('message', msg => {
       } else {
         msg.channel.send('Unexpected number of arguments. Example usage: "$gamble 100"');
       }
+      
+    } else if (msgContent.includes('give')) {
+      // Split up message into expected arguments
+      const msgArgs = msgContent.split(" ");
+      // Check to make sure the right amount of args were provided
+      if (msgArgs.length != 3) {
+        msg.channel.send('Unexpected number of arguments. Example usage: "$give @someone 100"');
+        return;
+      }
+
+      // Check that the last argument is a number
+      const giveAmount = parseInt(msgArgs[2]);
+      if (isNaN(giveAmount)) {
+        msg.channel.send('Provide a dollar amount to give. Example usage: "$give @someone 100"');
+        return;
+      }
+
+      // Check that someone was @'d
+      const recipients = msg.mentions.users;
+      if (recipients.size != 1) {
+        msg.channel.send('@ one person to give money to. Example usage: "$give @someone 100"');
+        return;
+      }
+      const recipient = msg.mentions.users.values().next().value; // Of type: User
+
+      // Check to make sure someone didn't @ themselves
+      if (recipient.id === msg.author.id) {
+        msg.channel.send('You can\'t send money to yourself.');
+        return;
+      }
+
+      // Check to see if the recipient has an account
+      client.query('select currency from currency where id=$1', [recipient.id], (err, res) => {
+        if (err) {
+          console.log(new Date().toISOString() + ' -- [ERROR]', err);
+          msg.channel.send('Something went wrong. Check the console.');
+          return;
+        }
+
+        // If the recipient doesn't have an account...
+        if (res.rowCount <= 0) {
+          msg.channel.send(`${recipient.username} doesn't have an account. Ask them to make one with \`$bank\``);
+          return;
+        }
+
+        const recipientOldBalance = parseInt(res.rows[0].currency);
+
+        // Check to see if giver has enough money
+        client.query('select currency from currency where id=$1', [msg.author.id], (err, res) => {
+          if (err) {
+            console.log(new Date().toISOString() + ' -- [ERROR]', err);
+            msg.channel.send('Something went wrong. Check the console.');
+            return;
+          }
+
+          // If the giver doesn't have an account...
+          if (res.rowCount <= 0) {
+            msg.channel.send('You need to make an account before you can give someone else money.');
+            return;
+          }
+
+          // Make sure transaction won't let giver go below the balance floor
+          const giverOldBalance = parseInt(res.rows[0].currency);
+          if (giverOldBalance - giveAmount < balanceFloor) {
+            msg.channel.send(`That'd leave you with \$${giverOldBalance - giveAmount}. You can't go below $10`);
+            return;
+          }
+
+          // Process the transaction
+          const recipientNewBalance = recipientOldBalance + giveAmount;
+          const giverNewBalance = giverOldBalance - giveAmount;
+          client.query('update currency set currency=$1 where id=$2;', [recipientNewBalance, recipient.id], (err, res) => {
+            if (err) {
+              console.log(new Date().toISOString() + ' -- [ERROR]', err);
+              msg.channel.send('Something went wrong. Check the console.');
+              return;
+            }
+          });
+          client.query('update currency set currency=$1 where id=$2;', [giverNewBalance, msg.author.id], (err, res) => {
+            if (err) {
+              console.log(new Date().toISOString() + ' -- [ERROR]', err);
+              msg.channel.send('Something went wrong. Check the console.');
+              return;
+            }
+          });
+          msg.channel.send(`You gave ${recipient.username} \$${giveAmount}. Their new balance is \$${recipientNewBalance}; your new balance is \$${giverNewBalance}`);
+        });
+      });
     } else {
       // Someone's message began with the bot's prefix, but we don't have
       // instructions for handling what was written after that
