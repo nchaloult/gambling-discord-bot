5fb462087719459c8cf7fd807d096984fd085311
Nick Chaloult <npchaloult@gmail.com>
2020-02-01 04:01:00 -0500

    Added timestamps to log messages

--- index.js
+++ index.js
@@ -25,10 +25,10 @@ dcClient.on('message', msg => {
     msgContent = msgContent.substring(prefix.length);
 
     if (msgContent === 'all') {
-      console.log('[INFO] Bot invoked with "all" message. Reading everyone\'s balance....');
+      console.log(new Date().toISOString() + ' -- [INFO] Bot invoked with "all" message. Reading everyone\'s balance....');
       client.query('select currency, username from currency;', (err, res) => {
         if (err) {
-          console.log('[ERROR]', err);
+          console.log(new Date().toISOString() + ' -- [ERROR]', err);
           msg.channel.send('Something went wrong. Check the console.');
           return;
         }
@@ -39,18 +39,18 @@ dcClient.on('message', msg => {
         msg.channel.send(output);
       });
     } else if (msgContent === 'bank') {
-      console.log(`[INFO] Bot invoked with "bank" message by ${msg.author.username}.`);
+      console.log(new Date().toISOString() + ` -- [INFO] Bot invoked with "bank" message by ${msg.author.username}.`);
       client.query('select currency from currency where id=$1;', [msg.author.id], (err, res) => {
         if (err) {
-          console.log('[ERROR]', err);
+          console.log(new Date().toISOString() + ' -- [ERROR]', err);
           msg.channel.send('Something went wrong. Check the console.');
           return;
         }
         if (res.rowCount > 0) {
-          console.log(`[INFO] Fetching ${msg.author.username}'s balance....`);
+          console.log(new Date().toISOString() + ` -- [INFO] Fetching ${msg.author.username}'s balance....`);
           client.query('select currency from currency where id=$1;', [msg.author.id], (err, res) => {
             if (err) {
-              console.log('[ERROR]', err);
+              console.log(new Date().toISOString() + ' -- [ERROR]', err);
               msg.channel.send('Something went wrong. Check the console.');
               return;
             }
@@ -58,16 +58,16 @@ dcClient.on('message', msg => {
             msg.reply(`your balance is: \$${res.rows[0].currency}`);
           });
         } else {
-          console.log(`[INFO] Creating an account for ${msg.author.username}...`);
+          console.log(new Date().toISOString() + ` -- [INFO] Creating an account for ${msg.author.username}...`);
           msg.channel.send('Making an account for you...');
           client.query('insert into currency (id, username) values ($1, $2);', [msg.author.id, msg.author.username], (err, res) => {
             if (err) {
-              console.log('[ERROR]', err);
+              console.log(new Date().toISOString() + ' -- [ERROR]', err);
               msg.channel.send('Something went wrong while trying to make your account. Check the console.');
               return;
             }
           });
-          console.log(`[INFO] Account created for ${msg.author.username}.`);
+          console.log(new Date().toISOString() + ` -- [INFO] Account created for ${msg.author.username}.`);
           msg.channel.send(`You have an account now :thumbsup: Type \`${prefix}bank\` to see your balance.`);
         }
       });
